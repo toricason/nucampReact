bdc2f764da793242e6abd7955fb81bf7ca9c687e
Tori <torihoward27@yahoo.com>
2023-08-12 22:41:49 -0400

    Comments

---
+++ src/utils/formatDate.js
@@ -0,0 +1,8 @@
+
+export const formatDate = (date) => {
+    return new Intl.DateTimeFormat('en-US', {
+        year: 'numeric',
+        month: 'short',
+        day: '2-digit'
+    }).format(new Date(Date.parse(date)));
+};
