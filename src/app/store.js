584fad5d802bceebc6c795d507c1257a96e8e743
Tori <torihoward27@yahoo.com>
2023-08-23 16:10:27 -0400

    Add Middleware

--- src/app/store.js
+++ src/app/store.js
@@ -1,15 +1,17 @@
 import { configureStore } from '@reduxjs/toolkit';
-import counterReducer from '../features/counter/counterSlice';
 import { campsitesReducer } from '../features/campsites/campsitesSlice';
 import {commentsReducer} from '../features/comments/commentsSlice';
 import {partnersReducer} from '../features/partners/partnersSlice';
 import {promotionsReducer} from '../features/promotions/promotionsSlice';
+import logger from 'redux-logger';
 
 export const store = configureStore({
   reducer: {
-    campsites: campsitesReducer,
-    comments: commentsReducer,
-    partners: partnersReducer,
-    promotions: promotionsReducer
-  },
+      campsites: campsitesReducer,
+      comments: commentsReducer,
+      partners: partnersReducer,
+      promotions: promotionsReducer
+    },
+    middleware: (getDefaultMiddleware) => 
+      getDefaultMiddleware().concat([logger])
 });
