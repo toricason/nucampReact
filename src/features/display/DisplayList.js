969435b11f30af56d5a8595822fb328ac243d171
Tori <torihoward27@yahoo.com>
2023-09-06 15:29:31 -0400

    Async DisplayList

--- src/features/display/DisplayList.js
+++ src/features/display/DisplayList.js
@@ -3,6 +3,8 @@ import AnimatedDisplayCard from './AnimatedDisplayCard';
 import { selectFeaturedCampsite } from '../campsites/campsitesSlice';
 import { selectFeaturedPromotion } from '../promotions/promotionsSlice';
 import { selectFeaturedPartner } from '../partners/partnersSlice';
+import Error from '../../components/Error';
+import Loading from '../../components/Loading';
 import { useSelector } from 'react-redux';
 
 const DisplayList = () => {
@@ -16,10 +18,18 @@ const DisplayList = () => {
     return (
         <Row>
             {items.map((item, idx) => {
+                const { featuredItem, isLoading, errMsg } = item;
+
+                if (isLoading) {
+                    return <Loading key={idx} />;
+                }
+                if (errMsg) {
+                    return <Error errMsg={errMsg} key={idx} />;
+                }
                 return (
-                    item && (
+                    featuredItem && (
                         <Col md className='m-1' key={idx}>
-                            <AnimatedDisplayCard item={item} />
+                            <AnimatedDisplayCard item={featuredItem} />
                         </Col>
                     )
                 );
